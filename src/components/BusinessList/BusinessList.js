3e3ac319946ae19a70568e4dee672589f53bd9b5
Anthony Edwards <anthonyedwards@Anthonys-MacBook-Pro.local>
2018-02-11 10:36:08 -0700

    adding in props to support the app.

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -4,13 +4,12 @@ import React from 'react';
 class BusinessList extends React.Component {
 	render() {
 		return (
-			<div className="BusinessList">
-				<Business />
-				<Business />
-				<Business />
-				<Business />
-				<Business />
-				<Business />
+			<div className="BusinessList"> 
+				{
+					this.props.businesses.map(business => {
+  						return <Business businesses={business} />;
+					})
+				}
 			</div>
 		);
 	}
